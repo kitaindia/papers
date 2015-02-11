fe00a10c03d3b71b9217e6edd5c0f90692168de3
kobake <kobake@users.sourceforge.net>
2015-02-11 13:26:55 +0900

    登記時書類等

---
+++ data/haraikomi_shomeisho.php
@@ -0,0 +1,36 @@
+<?php
+
+// 設定
+define('DOC_NAME', 'haraikomi_shomeisho');
+
+// Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class){
+	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+require_once(dirname(__FILE__) . '/../lib/markdown.php');
+require_once(dirname(__FILE__) . '/../lib/php-markdown/Michelf/MarkdownExtra.inc.php');
+
+
+// Get Markdown class
+// use \Michelf\Markdown;
+use \Michelf\MarkdownExtra;
+
+# MarkDown
+$text = file_get_contents(dirname(__FILE__) . '/' . DOC_NAME . '.md');
+
+# 変換
+$html = MarkdownExtra::defaultTransform($text);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="<?php echo DOC_NAME; ?>.css" />
+		<title><?php echo DOC_NAME; ?></title>
+	</head>
+	<body>
+	<?php echo $html; ?>
+	</body>
+</html>
---
+++ update_pdf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wkhtmltopdf http://hogen-teikan/teikan.php teikan.pdf
+wkhtmltopdf http://hogen-teikan/hokkinin_ketteisho.php hokkinin_ketteisho.pdf
+wkhtmltopdf http://hogen-teikan/shunin_shodakusho.php shunin_shodakusho.pdf
+wkhtmltopdf http://hogen-teikan/haraikomi_shomeisho.php haraikomi_shomeisho.pdf
