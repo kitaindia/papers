ae5497d4448904f6e3408920f377e58810b9041d
kobake <kobake@users.sourceforge.net>
2015-03-14 17:35:00 +0900

    フォルダ名変更

--- update_pdf.sh
+++ update_pdf.sh
@@ -3,19 +3,19 @@
 #
 # md2, css -> html, css
 #
-./lib/md2html ./1.md/teikan.md              > ./2.html/teikan.html
-cp            ./1.md/teikan.css               ./2.html/teikan.css
-./lib/md2html ./1.md/haraikomi_shomeisho.md > ./2.html/haraikomi_shomeisho.html
-cp            ./1.md/haraikomi_shomeisho.css  ./2.html/haraikomi_shomeisho.css
-./lib/md2html ./1.md/hokkinin_ketteisho.md  > ./2.html/hokkinin_ketteisho.html
-cp            ./1.md/hokkinin_ketteisho.css   ./2.html/hokkinin_ketteisho.css
-./lib/md2html ./1.md/shunin_shodakusho.md   > ./2.html/shunin_shodakusho.html
-cp            ./1.md/shunin_shodakusho.css    ./2.html/shunin_shodakusho.css
+./lib/md2html ./1_md/teikan.md              > ./2_html/teikan.html
+cp            ./1_md/teikan.css               ./2_html/teikan.css
+./lib/md2html ./1_md/haraikomi_shomeisho.md > ./2_html/haraikomi_shomeisho.html
+cp            ./1_md/haraikomi_shomeisho.css  ./2_html/haraikomi_shomeisho.css
+./lib/md2html ./1_md/hokkinin_ketteisho.md  > ./2_html/hokkinin_ketteisho.html
+cp            ./1_md/hokkinin_ketteisho.css   ./2_html/hokkinin_ketteisho.css
+./lib/md2html ./1_md/shunin_shodakusho.md   > ./2_html/shunin_shodakusho.html
+cp            ./1_md/shunin_shodakusho.css    ./2_html/shunin_shodakusho.css
 
 #
 # html -> pdf
 #
-wkhtmltopdf ./2.html/teikan.html              ./3.pdf/teikan.pdf
-wkhtmltopdf ./2.html/hokkinin_ketteisho.html  ./3.pdf/hokkinin_ketteisho.pdf
-wkhtmltopdf ./2.html/shunin_shodakusho.html   ./3.pdf/shunin_shodakusho.pdf
-wkhtmltopdf ./2.html/haraikomi_shomeisho.html ./3.pdf/haraikomi_shomeisho.pdf
+wkhtmltopdf ./2_html/teikan.html              ./3_pdf/teikan.pdf
+wkhtmltopdf ./2_html/hokkinin_ketteisho.html  ./3_pdf/hokkinin_ketteisho.pdf
+wkhtmltopdf ./2_html/shunin_shodakusho.html   ./3_pdf/shunin_shodakusho.pdf
+wkhtmltopdf ./2_html/haraikomi_shomeisho.html ./3_pdf/haraikomi_shomeisho.pdf
